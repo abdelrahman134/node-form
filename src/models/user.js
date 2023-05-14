4413da73226f83aa3489b2c98320562eb85e0a4b
abdelrahman134 <104602156+abdelrahman134@users.noreply.github.com>
2023-05-14 13:40:33 +0300

    first commit

--- src/models/user.js
+++ src/models/user.js
@@ -1,75 +1,82 @@
 const mongoose=require('mongoose')
 const validator=require('validator')
-const bcryptjs = require ('bcryptjs')
-const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    require: true,
-    trim: true,
+const jwt=require('jsonwebtoken')
+const bcryptjs=require('bcryptjs')
+const userSchema=mongoose.Schema({
+  username:{
+    type:String,
+    required:true,
+    trim:true
   },
-  password: {
-    type: String,
-    required: true,
-    trim: true,
-    minlength: 8,
-    validate(value) {
-      let password = new RegExp(
-        "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])"
-      );
-      if (!password.test(value)) {
-        throw new Error(
-          "Password must include uppercase , lowercase , numbers , speacial characters"
-        );
+  password:{
+    type:String,
+    trim:true,
+    required:true,
+    validate(val){
+      const password=  new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])");
+      if(!password.test(val)){
+       throw new Error ('the apssword must contain upper and lower case letter')
       }
-    },
+    }
   },
-  email: {
-    type: String,
-    require: true,
-    trim: true,
-    validate(val) {
-      if (!validator.isEmail(val)) {
-        throw new Error("EMAIL IS INVALID");
-      }
-    },
-  },
-  age: {
-    type: Number,
-    trim: true,
-    default: 18,
-    validate(val) {
-      if (val <= 0) {
-        throw new Error("AGE must be positive");
-      }
-    },
-  },
-});
+email:{
+  type:String,
+  required:true,
+  trim:true,
+  validate(val){
+    if(!validator.isEmail(val)){
+       throw new Error ('THIS EMAIL IS INVALID')
+    }
+  }
+},
+age:{
+  type:Number,
+  default:18,
+  validate(val){
+    if(val<=0){
+      throw new Error('the age must greater than 0')
+    }
+  }
+},
+tokens:[{
+  type:String,
+  required:true
+}]
 
-userSchema.pre('save', async function(){
+})
+userSchema.pre('save',async function(){
   const user=this
   if(user.isModified('password')){
-    user.password = await bcryptjs.hash(user.password,8)
+    user.password=await bcryptjs.hash(user.password,8)
   }
-})
+} )
 
-
-userSchema.statics.findByCredentials = async (usern, pass) => {
-  const user = await User.findOne({ username: usern});
-  if (!user) {
-    throw new Error("Unable to login");
+userSchema.statics.findByCredentials=async (em,pass)=>{
+  const user= await User.findOne({email:em})
+  if(!user){
+    throw new Error('can not find the user')
   }
-
-  const isMatch = await bcryptjs.compare(pass, user.password);
-
-  if (!isMatch) {
-    throw new Error("Unable to login");
+  const isMatch = await bcryptjs.compare(pass,user.password)
+  if(!isMatch){
+    throw new Error('the password of email do not match')
+  
   }
-  return user;
+  return user
 };
+userSchema.methods.generateToken=async function(){
+ const user=this
+ const token =jwt.sign({_id:user._id.toString},"token200")
+ user.tokens=user.tokens.concat(token) 
+ await user.save()
+ return token
+}
+userSchema.methods.toJSON= function(){
+  const user=this
+  const userObject = user.toObject()
+  delete userObject.password
+  delete userObject.tokens
+  return userObject
+}
 
-const User=mongoose.model('User',userSchema)
-module.exports=User
-
-
-
-
+const User =mongoose.model("User",userSchema)
+module.exports = User
--- src/routers/user.js
+++ src/routers/user.js
@@ -8,62 +8,83 @@ router.post("/users", (req, res) => {
     .then((user) => res.status(200).send(user))
     .catch((e) => res.status(400).send(e));
 });
-router.post("/login", async (req, res) => {
-  try {
-    const user = await User.findByCredentials(
-      req.body.username,
-      req.body.password
-    );
- 
-    res.status(200).send({user});
-  } catch (e) {
-    res.status(400).send(e.message);
-  }
-});
 router.get("/users", (req, res) => {
   User.find({})
-    .then((users) => res.status(200).send(users))
+    .then((user) => res.status(200).send(user))
     .catch((e) => res.status(400).send(e));
 });
-router.get('/users/:id',async(req,res)=>{
+router.get("/users/:id",async(req,res)=>{
   try{
     const _id=req.params.id
+     const user=await User.findById(_id)
+     if(!user){
+      throw new Error ("can not find the user")
+
+     }
+     res.status(200).send(user)
+  }catch(e){
+    res.status(400).send(e)
+  }
+})
+router.patch("/users/:id",async(req,res)=>{
+  try{
+    const _id=req.params.id
+    const updates=Object.keys(req.body)
     const user=await User.findById(_id)
     if(!user){
-      throw new Error('can not find the user')
+      throw new Error ("the user is invalid")
     }
-    res.status(200).send(user)
-  }catch(e){
-    res.status(400).send(e)
-  }
-})
-router.patch('/users/:id',async(req,res)=>{
-  try{
-    const _id=req.params.id
-    const user=await User.findById(_id)
-     if (!user) {
-       return res.status(404).send("No user is found");
-     }
-
-    const update=Object.keys(req.body)
-    update.forEach(ele=>user[ele]=req.body[ele])
-  await  user.save()
-    res.status(200).send(user)
+    updates.forEach(ele=>user[ele]==req.body[ele])
+    user.save()
+    req.status(200).send(user)
   }catch(e){
     res.status(400).send(e)
   }
 })
 
-router.delete("/users/:id", async (req, res) => {
+router.delete("/users/:id",async(req,res)=>{
   try {
-    const _id = req.params.id;
-    const user = await User.findByIdAndDelete(_id);
-    if (!user) {
-      return res.status(404).send("Unable to find user");
-    }
-    res.status(200).send(user);
+     const _id=req.params.id
+     const user=await User.findByIdAndDelete(_id)
+     if(!user){
+      throw new Error("wrong id")
+     }
+res.status(200).send(user)
+  } catch (e) {
+    res.status(400).send(e);
+  }
+})
+
+router.post("/login",async(req,res)=>{
+  try {
+     const user = await User.findByCredentials(req.body.email,req.body.password);
+     const token = await user.generateToken();
+     user.save()
+     res.status(200).send([user,token])
+  } catch (e) {
+    res.status(400).send(e);
+  }
+})
+router.post("/users",async(req,res)=>{
+  try{
+     const user=new User(req.body)
+     const token=await user.generateToken()
+     user.save()
+     res.status(200).send(user)
+  }catch(e){
+
+  }
+})
+router.post("/logout", async (req, res) => {
+  try {
+    req.body.tokens = req.body.tokens.filter((token) => {
+      return token.token !== req.token;
+    });
+    await req.body.save();
+    res.status(200).send();
   } catch (e) {
     res.status(400).send(e);
   }
 });
+
 module.exports=router
